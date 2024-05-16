c0f9ec4e2c9ec5ae1475a5c946088e53e3f8f7fc
bayous3 <sebia.baya@outlook.com>
2024-05-16 22:18:53 +0200

    app git page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Projets from './pages/Projets/Projets'
-import Contact from './pages/Contact/Contact'
-import Error from './pages/Error/Error'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Projets from './pages/Projets/Projets';
+import Contact from './pages/Contact/Contact';
+import Error from './pages/Error/Error';
 
 function App() {
     return (
         <div className="App">
-          <React.StrictMode>
-            <Router>
-            <Routes>
-              <Route path="/" element={<Navigate to="portfolio"/>}/>
-              <Route path="/portfolio" element={<Home/>}/>
-              <Route path="/about" element={<About/>}/>
-              <Route path="/projets" element={<Projets/>}/>
-              <Route path="/contact" element={<Contact/>}/>
-              <Route path="*" element={<Error/>}/>
-            </Routes>
-            </Router>
-          </React.StrictMode>
+            <React.StrictMode>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/portfolio_baya" />} />
+                        <Route path="/portfolio" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/projets" element={<Projets />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="*" element={<Error />} />
+                    </Routes>
+                </Router>
+            </React.StrictMode>
         </div>
-      );
+    );
 }
 
 export default App;
