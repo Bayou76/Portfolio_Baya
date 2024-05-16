773badb8f2cbe9159bc5535ab69918fc175b83b4
bayous3 <sebia.baya@outlook.com>
2024-05-16 22:34:11 +0200

    ;;;

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                 <Router>
                     <Routes>
                         <Route path="/" element={<Navigate to="/Portfolio_Baya" />} />
-                        <Route path="/portfolio_baya" element={<Home />} />
+                        <Route path="/Portfolio_Baya" element={<Home />} />
                         <Route path="/about" element={<About />} />
                         <Route path="/projets" element={<Projets />} />
                         <Route path="/contact" element={<Contact />} />
